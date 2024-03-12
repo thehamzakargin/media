4a6d7bdfdc539138272ecd55cf787bd429495006
thehamzakargin <hamzakargin@outlook.com>
2024-03-12 17:27:07 +0300

    pull and add albums with RTK

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,52 @@
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandAblePanel from "./ExpandAblePanel";
+import Button from "./Button";
+
+//dev onlyy
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
+};
+
 function AlbumList({ user }) {
-    return <div>Albums for {user.name}</div>
+    const { data, error, isLoading} = useFetchAlbumsQuery(user);
+    const [addAlbum, results] = useAddAlbumMutation();
+
+    const handleAddAlbum = () => {
+        addAlbum(user);
+    };
+
+
+
+    let content;
+    if(isLoading)  {
+        content = <Skeleton className="h-100 w-full" times={3}/>
+    }else if (error) {
+        content = <div> error loading albums.</div>
+    }else {
+        content = data.map(album => {
+            const header = <div>{album.title}</div>;
+
+            return <ExpandAblePanel key={album.id} header={header}>
+                list of photos in the album
+            </ExpandAblePanel>
+
+        });
+    }
+
+    //console.log(data, error, isLoading)
+
+    return (
+    <div>
+        <div className="m-2 flex-row items-center justify-between">
+            <h3 className="text-lg font-bold">Albums for {user.name}</h3>        
+            <Button loading={isLoading} onClick={handleAddAlbum}>+ add album</Button>
+        </div>        
+        <div>{content}</div>
+    </div>
+    )
 };
 
 export default AlbumList;
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./silces/usersSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { albumsApi } from "./apis/albumsApi";
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
+        [albumsApi.reducerPath]: albumsApi.reducer
     },
+    middleware:  (getDefaultMiddleware) => {
+        return getDefaultMiddleware()
+        .concat(albumsApi.middleware);
+    }
 });
+
+setupListeners(store.dispatch);
 
 export * from './thunk/fetchUsers';
 export * from './thunk/addUser';
 export * from './thunk/removeUser';
+export { useFetchAlbumsQuery, useAddAlbumMutation } from './apis/albumsApi';
 
 // exporttan sonraki yildizin anlami gosterilen dosyanin icindenki exportu ayni sekilde bu dosyayada aktarmak anlamina geliyor sonrasinda bu dosyadan da export ediyoruz
